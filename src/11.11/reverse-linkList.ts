e2566732aa0399e531ad7521c21010d931bf34f6
fengxie <1015806955@qq.com>
2023-11-11 20:32:39 +0800

    翻转链表

---
+++ src/11.11/reverse-linkList.ts
@@ -0,0 +1,14 @@
+/**
+ * @description 反转单向链表
+ *
+ */
+
+interface ILinkeListNode {
+  value: number;
+  next?: ILinkeListNode;
+  prev?: ILinkeListNode;
+}
+
+function createLinkList(arr: number[]): ILinkeListNode[] {
+  return [];
+}
